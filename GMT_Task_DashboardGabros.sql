2a53faf14c78835fedd6527f41ee9822f0b6691f
NGUYỄN THỊ HIỀN <114793644+nguyenhienneu0706@users.noreply.github.com>
2023-06-07 14:29:38 +0700

    Update GMT_Task_DashboardGabros.sql

--- GMT_Task_DashboardGabros.sql
+++ GMT_Task_DashboardGabros.sql
@@ -7,4 +7,8 @@ BI A Đức ✈, [6/7/2023 2:20 PM]
 - thì nó vẫn sẽ lưu ở intraday. Sau 3 ngày nó sẽ đẩy về event 
 */
 
-SELECT 
+-- CHÚ Ý: TÊN BẢNG EVENTS_ SẼ LÀ ĐƯỢC CẬP NHẬT HÀNG NGÀY. VÍ DỤ ***.events_20230606 THÌ 20230606 SẼ LÀ NGÀY GẦN NHẤT CÓ DỮ LIỆU XUẤT ĐƯỢC CẬP NHẬT TRONG BẢNG:
+-- Vì vậy khi sử dụng nên copy ID của bảng để đúng nhất:
+SELECT * 
+FROM `gmogamesdk5.analytics_165902128.events_20230606`
+LIMIT 5
