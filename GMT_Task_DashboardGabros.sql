583b24c165cac6198fc42c8764f89cbac46e147e
NGUYỄN THỊ HIỀN <114793644+nguyenhienneu0706@users.noreply.github.com>
2023-06-07 14:25:22 +0700

    Update GMT_Task_DashboardGabros.sql

--- GMT_Task_DashboardGabros.sql
+++ GMT_Task_DashboardGabros.sql
@@ -1,1 +1,10 @@
+/* 
+Task sử dụng dữ liệu ở Dataset: gmogamesdk5.analytics_165902128. Table Event với event_intraday là kéo tự động từ Firebase Analytics ra BigQuery:
+BI A Đức ✈, [6/7/2023 2:20 PM]
+- intraday là kéo realtime trong ngày
+- kéo tự động từ firebase sang
+- có một số data về chậm, cụ thể là 3 ngày sau mới về
+- thì nó vẫn sẽ lưu ở intraday. Sau 3 ngày nó sẽ đẩy về event 
+*/
+
 SELECT 
