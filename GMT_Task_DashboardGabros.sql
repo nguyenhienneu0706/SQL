f87ad5fb56a2965fd00bc2a465fd9b7bf62a3f8e
NGUYỄN THỊ HIỀN <114793644+nguyenhienneu0706@users.noreply.github.com>
2023-06-08 10:49:47 +0700

    Update GMT_Task_DashboardGabros.sql

--- GMT_Task_DashboardGabros.sql
+++ GMT_Task_DashboardGabros.sql
@@ -10,5 +10,5 @@ BI A Đức ✈, [6/7/2023 2:20 PM]
 -- CHÚ Ý: TÊN BẢNG EVENTS_ SẼ LÀ ĐƯỢC CẬP NHẬT HÀNG NGÀY. VÍ DỤ ***.events_20230606 THÌ 20230606 SẼ LÀ NGÀY GẦN NHẤT CÓ DỮ LIỆU XUẤT ĐƯỢC CẬP NHẬT TRONG BẢNG:
 -- Vì vậy khi sử dụng nên copy ID của bảng để đúng nhất:
 SELECT * 
-FROM `gmogamesdk5.analytics_165902128.events_20230606`
-LIMIT 5
+FROM `gab002.analytics_378566684.events_20230606`
+LIMIT 3
