629e32f718d9c341090e228aa6ecc4dbd70e2572
NGUYỄN THỊ HIỀN <114793644+nguyenhienneu0706@users.noreply.github.com>
2023-06-12 00:38:37 +0700

    Update GMT_Task_Trung vị New user ACT.sql

--- GMT_Task_Trung vị New user ACT.sql
+++ GMT_Task_Trung vị New user ACT.sql
@@ -91,6 +91,21 @@ WHERE trans_dedup.user_id IN(SELECT user_sum.user_id FROM user_sum)
 -----------------------------------------------------------------------------------------------------------------------------------------------------
 
 -- TASK TRUNG ORDER NOTION: Tìm mốc nạp trung vị (nếu có kĩ từng percentile thì càng tốt) của NEW USER Alo Chủ Tướng trong giai đoạn OB-OB+30 và 02/06-09/06:
+-- Check: LẤY RA DOANH THU THEO NGÀY:
+WITH A AS (
+  SELECT
+    DISTINCT (transaction.id) AS Transaction_ID,
+    DATE,
+    transaction.amount_local AS Amount_local
+  FROM `gamotasdk5.bidata.transactions`
+  WHERE app.game_id = 180941 AND (Date <= "2023-05-27" AND Date >= "2023-04-27")
+  AND transaction.vendor != "gamota_tester"
+)
+SELECT
+  A.Date,
+  SUM(A.Amount_local) AS Total_Amount_local
+FROM A
+GROUP BY A.DATE;
 
 -- Lấy ra danh sách nạp (transactions) không bị dup dữ liệu OB-OB+30
 SELECT
@@ -156,4 +171,37 @@ LEFT JOIN REGISTER AS R ON T.T_User_ID = R.R_User_ID
 --- LẤY ĐIỀU KIỆN ĐĂNG KÍ TRONG KHOẢNG 02-09/06 TỨC LÀ NRU TRONG KHOẢNG THGIAN NÀY:
 WHERE R.First_date_register_roles <= "2023-06-09" AND R.First_date_register_roles >= "2023-06-02"
     AND R.R_User_ID IS NOT NULL; -- LOẠI ĐI NHỮNG NGƯỜI NẠP TRONG THGIAN NÀY NHƯNG LÀ OLD USER RỒI
+    
+-- Lấy ra trung vị:
+WITH TRANS_ALO AS
+(
+  SELECT
+  DISTINCT (transaction.id) AS Transaction_ID,
+  user.user_id AS T_User_ID,
+  date AS Date_pay,
+  transaction.amount_local AS Amount_local
+FROM `gamotasdk5.bidata.transactions`
+WHERE app.game_id = 180941 AND (DATE(Date) <= "2023-06-09" AND DATE(Date) >= "2023-06-02")
+AND transaction.vendor != "gamota_tester"
+),
+--- LẤY RA THÔNG TIN USER ĐĂNG KÍ GAME ALO:
+REGISTER AS
+(
+  SELECT
+  user_id AS R_User_ID,
+  Date AS Date_register_roles,
+  MIN(date) AS First_date_register_roles
+FROM `gamotasdk5.bidata.register_logs`
+WHERE game_id = 180941
+GROUP BY user_id, Date
+)
+SELECT
+  APPROX_QUANTILES(Amount_local, 100)[OFFSET(9)] AS percentile_10,
+  APPROX_QUANTILES(Amount_local, 100)[OFFSET(24)] AS percentile_25,
+  APPROX_QUANTILES(Amount_local, 100)[OFFSET(39)] AS percentile_40,
+  APPROX_QUANTILES(Amount_local, 100)[OFFSET(49)] AS percentile_50,
+  APPROX_QUANTILES(Amount_local, 100)[OFFSET(59)] AS percentile_60,
+  APPROX_QUANTILES(Amount_local, 100)[OFFSET(74)] AS percentile_75,
+  APPROX_QUANTILES(Amount_local, 100)[OFFSET(89)] AS percentile_90,
+FROM TRANS_ALO;
 
