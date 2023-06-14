af98ca083a46b632cb5f19b8aa6a27f8053a4154
NGUYỄN THỊ HIỀN <114793644+nguyenhienneu0706@users.noreply.github.com>
2023-06-14 22:34:06 +0700

    Update GMT_Task_UserStikiness.sql

--- GMT_Task_UserStikiness.sql
+++ GMT_Task_UserStikiness.sql
@@ -121,7 +121,7 @@ WHERE game_id = 180941 AND DATE(Date) >= "2023-04-27" AND server_id != "001"
 SELECT LOGIN.Date_login, SERVER.Date_server, SERVER.server_id, LOGIN.user_id AS User_ID
 FROM LOGIN 
 LEFT JOIN SERVER ON LOGIN.user_id = SERVER.user_id AND LOGIN.Date_login = SERVER.Date_server
--- WHERE LOGIN.Date_login = "2023-04-30" ; ví dụ check ngày 30/05
+WHERE LOGIN.Date_login = "2023-04-30" ; -- ví dụ check ngày 30/05
 
 --- Ngoài ra dữ liệu từ bảng roles chỉ ghi nhận được như này (thiếu đi những người login nhưng k tracking được server_id)
 WITH A AS (
@@ -136,9 +136,8 @@ ORDER BY Date_server;
 --- Thực sự, các users này có login nhưng k có thông tin về server:
 SELECT *
 FROM `gamotasdk5.bidata.game_roles`
-WHERE game_id = 180941 AND DATE(Date) >= "2023-04-27" AND server_id IN ("15812993",
-"65923379",
-"58960807",
-"66202958",
-"66213370")
----> There is no data to display.
+WHERE game_id = 180941 AND DATE(Date) = "2023-04-30" AND user_id IN (15812993,
+66213370,
+65923379,
+58960807)
+---> Chỉ có user_id 65923379 có thông tin thôi, các user kia k tracking ra được
