5ac764edbeb6f0c7254a461261c0a9371df732c0
NGUYỄN THỊ HIỀN <114793644+nguyenhienneu0706@users.noreply.github.com>
2023-06-21 17:10:34 +0700

    Update GMT_GDP_Task_UserStikiness.sql

--- GMT_GDP_Task_UserStikiness.sql
+++ GMT_GDP_Task_UserStikiness.sql
@@ -84,3 +84,107 @@ A AS (
 )
 SELECT *
 FROM A;
+---------------------------------------------------------------------------------------------------------------------
+
+-- 1. LẤY RA ACTIVE USER:
+
+-- Thử bảng login_logs:
+WITH LOGIN AS (
+  SELECT userinfo.user_id, date AS login_date
+  FROM `gamotasdk5.bidata.login_logs`
+  WHERE appinfo.game_id = 180941 AND (Date <= "2023-05-26" AND Date >= "2023-04-28")
+  -- Mình muốn lấy Old User chỉ cần lấy những thằng đã từng có đăng nhập trước đó là được
+)
+SELECT LOGIN.login_date,
+  COUNT(DISTINCT user_id) AS AU
+FROM LOGIN 
+GROUP BY LOGIN.login_date
+ORDER BY login_date ASC;
+-- Kết quả trả về là Active User (check với Ana chỉ số DAU không chênh lệch nhiều)
+
+-- Sau đó tạo ra bằng chứa cả ngày login và ngày login đầu tiên bằng cách tìm ra ngày login đầu tiên của user đó, với điều kiện lần đăng nhập đầu tiên < khoảng thời gian là ok
+-- Bước 1: Tạo ra bảng chứa dữ liệu
+-- CÁCH 1:
+WITH FIRST_LOGIN AS (
+  SELECT userinfo.user_id, MIN(Date) AS first_login_date
+  FROM `gamotasdk5.bidata.login_logs`
+  WHERE appinfo.game_id = 180941 AND (Date <= "2023-05-26" AND Date >= "2023-04-27")
+  GROUP BY userinfo.user_id
+), LOGIN AS (
+  SELECT userinfo.user_id, Date AS login_date
+  FROM `gamotasdk5.bidata.login_logs`
+  WHERE appinfo.game_id = 180941 AND (Date <= "2023-05-26" AND Date >= "2023-04-27")
+)
+SELECT LOGIN.user_id, LOGIN.login_date, FIRST_LOGIN.first_login_date
+FROM LOGIN
+FULL JOIN FIRST_LOGIN ON FIRST_LOGIN.user_id = LOGIN.user_id;
+
+-- CÁCH 2:
+WITH FIRST_LOGIN AS (
+SELECT userinfo.user_id, MIN(Date) AS first_login_date
+FROM `gamotasdk5.bidata.login_logs`
+WHERE appinfo.game_id = 180941 AND (Date <= "2023-05-26" AND Date >= "2023-04-27")
+GROUP BY userinfo.user_id
+)
+SELECT LOGIN.userinfo.user_id, LOGIN.date, FIRST_LOGIN.first_login_date
+FROM `gamotasdk5.bidata.login_logs` AS LOGIN
+FULL JOIN FIRST_LOGIN ON FIRST_LOGIN.user_id = LOGIN.userinfo.user_id
+WHERE appinfo.game_id = 180941 AND (Date <= "2023-05-26" AND Date >= "2023-04-27");
+
+-- Bước 2: Lấy ra AOU
+-- Cách 1: (CHƯA TỐI ƯU TỐT)
+WITH A AS (
+  WITH FIRST_LOGIN AS (
+  SELECT userinfo.user_id, MIN(Date) AS first_login_date
+  FROM `gamotasdk5.bidata.login_logs`
+  WHERE appinfo.game_id = 180941 AND (Date <= "2023-05-26" AND Date >= "2023-04-27")
+  GROUP BY userinfo.user_id
+), LOGIN AS (
+  SELECT userinfo.user_id, Date AS login_date
+  FROM `gamotasdk5.bidata.login_logs`
+  WHERE appinfo.game_id = 180941 AND (Date <= "2023-05-26" AND Date >= "2023-04-27")
+)
+SELECT LOGIN.user_id, LOGIN.login_date, FIRST_LOGIN.first_login_date
+FROM LOGIN
+FULL JOIN FIRST_LOGIN ON FIRST_LOGIN.user_id = LOGIN.user_id
+)
+SELECT login_date, COUNT(DISTINCT user_id)
+FROM A
+WHERE login_date > first_login_date
+GROUP BY login_date;
+
+-- Cách 2:
+SELECT A.Date, COUNT(DISTINCT A.user_id)
+FROM
+(
+WITH FIRST_LOGIN AS (
+SELECT userinfo.user_id, MIN(Date) AS first_login_date
+FROM `gamotasdk5.bidata.login_logs`
+WHERE appinfo.game_id = 180941 AND (Date <= "2023-05-26" AND Date >= "2023-04-27")
+GROUP BY userinfo.user_id
+)
+SELECT LOGIN.userinfo.user_id, LOGIN.Date, FIRST_LOGIN.first_login_date
+FROM `gamotasdk5.bidata.login_logs` AS LOGIN
+FULL JOIN FIRST_LOGIN ON FIRST_LOGIN.user_id = LOGIN.userinfo.user_id
+WHERE appinfo.game_id = 180941 AND (Date <= "2023-05-26" AND Date >= "2023-04-27")
+) AS A
+WHERE A.Date > A.First_login_date
+GROUP BY A.Date;
+
+-- CÁCH 3: (Tối ưu nhất)
+WITH FIRST_LOGIN AS (
+SELECT userinfo.user_id, MIN(Date) AS first_login_date
+FROM `gamotasdk5.bidata.login_logs`
+WHERE appinfo.game_id = 180941 AND (Date <= "2023-05-26" AND Date >= "2023-04-27")
+GROUP BY userinfo.user_id
+),
+LOGIN AS (
+SELECT userinfo.user_id, Date, FIRST_LOGIN.first_login_date
+FROM `gamotasdk5.bidata.login_logs` 
+FULL JOIN FIRST_LOGIN ON FIRST_LOGIN.user_id = userinfo.user_id
+WHERE appinfo.game_id = 180941 AND (Date <= "2023-05-26" AND Date >= "2023-04-27")
+)
+SELECT A.Date, COUNT(DISTINCT A.user_id)
+FROM LOGIN AS A
+WHERE A.Date > A.First_login_date
+GROUP BY A.Date;
