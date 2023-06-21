c87dfb39f2eaf290daeac3892f8ae9bc54e2becc
NGUYỄN THỊ HIỀN <114793644+nguyenhienneu0706@users.noreply.github.com>
2023-06-21 18:08:47 +0700

    Update Learn_firebase-developers.sql

--- Learn_firebase-developers.sql
+++ Learn_firebase-developers.sql
@@ -148,3 +148,64 @@ FROM (
 -->
 
 ** TRƯỜNG HỢP TỔNG QUÁT:
+-- BƯỚC 01:
+WITH analytics_data AS (
+  SELECT user_pseudo_id, event_timestamp, event_name, 
+    UNIX_MICROS(TIMESTAMP("2018-08-01 00:00:00", "-7:00")) AS start_day,
+    3600*1000*1000*24*7 AS one_week_micros
+  FROM `firebase-public-project.analytics_153293282.events_*`
+  WHERE _table_suffix BETWEEN '20180731' AND '20180829'
+)
+SELECT DISTINCT user_pseudo_id
+FROM analytics_data
+WHERE event_name = 'first_open'
+  AND event_timestamp BETWEEN start_day AND start_day+(1*one_week_micros)
+
+-- BƯỚC 02:
+WITH analytics_data AS (
+  SELECT user_pseudo_id, event_timestamp, event_name, 
+    UNIX_MICROS(TIMESTAMP("2018-08-01 00:00:00", "-7:00")) AS start_day,
+    3600*1000*1000*24*7 AS one_week_micros
+  FROM `firebase-public-project.analytics_153293282.events_*`
+  WHERE _table_suffix BETWEEN '20180731' AND '20180829'
+)
+  
+SELECT week_0_cohort / week_0_cohort AS week_0_pct,
+ week_1_cohort / week_0_cohort AS week_1_pct,
+ week_2_cohort / week_0_cohort AS week_2_pct,
+ week_3_cohort / week_0_cohort AS week_3_pct
+FROM (
+  WITH week_3_users AS (
+    SELECT DISTINCT user_pseudo_id
+    FROM analytics_data
+    WHERE event_timestamp BETWEEN start_day+(3*one_week_micros) AND start_day+(4*one_week_micros)
+  ),
+  week_2_users AS (
+    SELECT DISTINCT user_pseudo_id
+    FROM analytics_data
+    WHERE event_timestamp BETWEEN start_day+(2*one_week_micros) AND start_day+(3*one_week_micros)
+  ),
+  week_1_users AS (
+    SELECT DISTINCT user_pseudo_id
+    FROM analytics_data
+    WHERE event_timestamp BETWEEN start_day+(1*one_week_micros) AND start_day+(2*one_week_micros)
+  ), 
+  week_0_users AS (
+    SELECT DISTINCT user_pseudo_id
+    FROM analytics_data
+    WHERE event_name = 'first_open'
+      AND event_timestamp BETWEEN start_day AND start_day+(1*one_week_micros)
+  )
+  SELECT 
+    (SELECT count(*) 
+     FROM week_0_users) AS week_0_cohort,
+    (SELECT count(*) 
+     FROM week_1_users 
+     JOIN week_0_users USING (user_pseudo_id)) AS week_1_cohort,
+    (SELECT count(*) 
+     FROM week_2_users 
+     JOIN week_0_users USING (user_pseudo_id)) AS week_2_cohort,
+    (SELECT count(*) 
+     FROM week_3_users 
+     JOIN week_0_users USING (user_pseudo_id)) AS week_3_cohort
+);
