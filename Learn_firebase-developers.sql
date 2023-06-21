d35ae8bf39f7da836d0cae480433d160adc1258d
NGUYỄN THỊ HIỀN <114793644+nguyenhienneu0706@users.noreply.github.com>
2023-06-21 17:24:03 +0700

    Update Learn_firebase-developers.sql

--- Learn_firebase-developers.sql
+++ Learn_firebase-developers.sql
@@ -10,7 +10,7 @@ https://towardsdatascience.com/a-complete-guide-to-cohort-analysis-using-bigquer
 ---------------------------------------------------------------------------------------------------------------------------------------------
 /* 1. PHỤC VỤ TÍNH RETENTION RATE (RR) */
 
--- Thực hiện:
+-- THỰC HIỆN BƯỚC 01: LẤY DANH SÁCH USER THỎA MÃN:
 -- CÁCH 01:
 -- Lấy ra danh sách người dùng đã sử dụng ứng dụng từ ngày 8/88 đến ngày 14/8 nằm trong nhóm người chơi bắt đầu sử dụng ứng dụng vào tuần trước.
 SELECT DISTINCT user_pseudo_id
@@ -49,3 +49,46 @@ week_0_users AS
     AND _TABLE_SUFFIX BETWEEN '20180731' AND '20180808'
 )
 SELECT * from week_1_users JOIN week_0_users USING(user_pseudo_id);
+
+-- THỰC HIỆN BƯỚC 02: LẤY THEO COHORT
+
+WITH week_3_users AS (
+  SELECT DISTINCT user_pseudo_id
+  FROM `firebase-public-project.analytics_153293282.events_*`
+  WHERE event_timestamp >= UNIX_MICROS(TIMESTAMP("2018-08-22 00:00:00", "-7:00")) 
+    AND  event_timestamp < UNIX_MICROS(TIMESTAMP("2018-08-29 00:00:00", "-7:00")) 
+    AND _TABLE_SUFFIX BETWEEN '20180821' AND '20180829'
+),
+week_2_users AS (
+  SELECT DISTINCT user_pseudo_id
+  FROM `firebase-public-project.analytics_153293282.events_*`
+  WHERE event_timestamp >= UNIX_MICROS(TIMESTAMP("2018-08-15 00:00:00", "-7:00")) 
+    AND  event_timestamp < UNIX_MICROS(TIMESTAMP("2018-08-22 00:00:00", "-7:00")) 
+    AND _TABLE_SUFFIX BETWEEN '20180814' AND '20180822'
+),
+week_1_users AS (
+  SELECT DISTINCT user_pseudo_id
+  FROM `firebase-public-project.analytics_153293282.events_*`
+  WHERE event_timestamp >= UNIX_MICROS(TIMESTAMP("2018-08-08 00:00:00", "-7:00")) 
+    AND  event_timestamp < UNIX_MICROS(TIMESTAMP("2018-08-15 00:00:00", "-7:00")) 
+    AND _TABLE_SUFFIX BETWEEN '20180807' AND '20180815'
+), week_0_users AS (
+  SELECT DISTINCT user_pseudo_id
+  FROM `firebase-public-project.analytics_153293282.events_*`
+  WHERE event_name = 'first_open'
+    AND event_timestamp >= UNIX_MICROS(TIMESTAMP("2018-08-01 00:00:00", "-7:00")) 
+    AND  event_timestamp < UNIX_MICROS(TIMESTAMP("2018-08-08 00:00:00", "-7:00")) 
+    AND _TABLE_SUFFIX BETWEEN '20180731' AND '20180808'
+)
+SELECT 
+  (SELECT count(*) FROM week_0_users) AS week_0_cohort,
+  (SELECT count(*) 
+   FROM week_1_users 
+   JOIN week_0_users USING (user_pseudo_id)) AS week_1_cohort,
+  (SELECT count(*) 
+   FROM week_2_users 
+   JOIN week_0_users USING (user_pseudo_id)) AS week_2_cohort,
+  (SELECT count(*) 
+   FROM week_3_users 
+   JOIN week_0_users USING (user_pseudo_id)) AS week_3_cohort;
+
