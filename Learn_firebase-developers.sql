e294147c51184386973df591003640cf2a1a5e75
NGUYỄN THỊ HIỀN <114793644+nguyenhienneu0706@users.noreply.github.com>
2023-06-21 17:20:21 +0700

    Update Learn_firebase-developers.sql

--- Learn_firebase-developers.sql
+++ Learn_firebase-developers.sql
@@ -7,5 +7,23 @@ https://medium.com/firebase-developers/how-to-use-select-from-unnest-to-analyze-
 https://towardsdatascience.com/a-complete-guide-to-cohort-analysis-using-bigquery-and-looker-studio-1cd18c0edd79
 ...
 */
+---------------------------------------------------------------------------------------------------------------------------------------------
+/* 1. PHỤC VỤ TÍNH RETENTION RATE (RR) */
 
--- 
+-- Thực hiện:
+-- Lấy ra danh sách người dùng đã sử dụng ứng dụng từ ngày 8/88 đến ngày 14/8 nằm trong nhóm người chơi bắt đầu sử dụng ứng dụng vào tuần trước.
+SELECT DISTINCT user_pseudo_id
+FROM `firebase-public-project.analytics_153293282.events_*`
+WHERE event_timestamp >= UNIX_MICROS(TIMESTAMP("2018-08-08 00:00:00", "-7:00")) 
+AND  event_timestamp < UNIX_MICROS(TIMESTAMP("2018-08-15 00:00:00", "-7:00")) 
+AND _TABLE_SUFFIX BETWEEN '20180807' AND '20180815'
+AND user_pseudo_id IN 
+( 
+  -- Đầu tiên lấy ra danh sách tất cả người dùng lần đầu tiên mở ứng dụng sau nửa đêm vào ngày 1/8 và ngay trước nửa đêm ngày 8/8:
+  SELECT DISTINCT user_pseudo_id
+  FROM `firebase-public-project.analytics_153293282.events_*`
+  WHERE event_name = 'first_open'
+  AND event_timestamp >= UNIX_MICROS(TIMESTAMP("2018-08-01 00:00:00", "-7:00")) 
+  AND  event_timestamp < UNIX_MICROS(TIMESTAMP("2018-08-08 00:00:00", "-7:00")) 
+  AND _TABLE_SUFFIX BETWEEN '20180731' AND '20180808'
+);
