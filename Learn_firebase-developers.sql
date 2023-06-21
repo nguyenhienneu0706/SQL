246f212ff85c005bddb560e7a83a8006cf2d67a1
NGUYỄN THỊ HIỀN <114793644+nguyenhienneu0706@users.noreply.github.com>
2023-06-21 17:21:43 +0700

    Update Learn_firebase-developers.sql

--- Learn_firebase-developers.sql
+++ Learn_firebase-developers.sql
@@ -11,6 +11,7 @@ https://towardsdatascience.com/a-complete-guide-to-cohort-analysis-using-bigquer
 /* 1. PHỤC VỤ TÍNH RETENTION RATE (RR) */
 
 -- Thực hiện:
+-- CÁCH 01:
 -- Lấy ra danh sách người dùng đã sử dụng ứng dụng từ ngày 8/88 đến ngày 14/8 nằm trong nhóm người chơi bắt đầu sử dụng ứng dụng vào tuần trước.
 SELECT DISTINCT user_pseudo_id
 FROM `firebase-public-project.analytics_153293282.events_*`
@@ -27,3 +28,24 @@ AND user_pseudo_id IN
   AND  event_timestamp < UNIX_MICROS(TIMESTAMP("2018-08-08 00:00:00", "-7:00")) 
   AND _TABLE_SUFFIX BETWEEN '20180731' AND '20180808'
 );
+
+-- CÁCH 02:
+-- Lấy ra danh sách người dùng đã sử dụng ứng dụng từ ngày 8/88 đến ngày 14/8 nằm trong nhóm người chơi bắt đầu sử dụng ứng dụng vào tuần trước.
+WITH week_1_users AS (
+  SELECT DISTINCT user_pseudo_id
+  FROM `firebase-public-project.analytics_153293282.events_*`
+  WHERE event_timestamp >= UNIX_MICROS(TIMESTAMP("2018-08-08 00:00:00", "-7:00")) 
+    AND  event_timestamp < UNIX_MICROS(TIMESTAMP("2018-08-15 00:00:00", "-7:00")) 
+    AND _TABLE_SUFFIX BETWEEN '20180807' AND '20180815'
+), 
+week_0_users AS 
+(
+-- Đầu tiên lấy ra danh sách tất cả người dùng lần đầu tiên mở ứng dụng sau nửa đêm vào ngày 1/8 và ngay trước nửa đêm ngày 8/8:
+  SELECT DISTINCT user_pseudo_id
+  FROM `firebase-public-project.analytics_153293282.events_*`
+  WHERE event_name = 'first_open'
+    AND event_timestamp >= UNIX_MICROS(TIMESTAMP("2018-08-01 00:00:00", "-7:00")) 
+    AND  event_timestamp < UNIX_MICROS(TIMESTAMP("2018-08-08 00:00:00", "-7:00")) 
+    AND _TABLE_SUFFIX BETWEEN '20180731' AND '20180808'
+)
+SELECT * from week_1_users JOIN week_0_users USING(user_pseudo_id);
