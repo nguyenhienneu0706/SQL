501c2d56d9395a96faa129260d6d3cad40397d47
NGUYỄN THỊ HIỀN <114793644+nguyenhienneu0706@users.noreply.github.com>
2023-06-21 16:56:41 +0700

    Create firebase-developers.sql

---
+++ firebase-developers.sql
@@ -0,0 +1,11 @@
+/* 
+Thông tin tài liệu được lấy tại: 
+https://medium.com/firebase-developers
+Ví dụ:
+https://medium.com/firebase-developers/analyzing-custom-retention-cohorts-using-bigquery-and-google-analytics-for-firebase-779777595950
+https://medium.com/firebase-developers/how-to-use-select-from-unnest-to-analyze-multiple-parameters-in-bigquery-for-analytics-5838f7a004c2
+https://towardsdatascience.com/a-complete-guide-to-cohort-analysis-using-bigquery-and-looker-studio-1cd18c0edd79
+...
+*/
+
+-- 
