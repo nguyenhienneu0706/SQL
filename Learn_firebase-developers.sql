052a8b7559ff56adcbf1d2d68cb2aca1e3ce23d4
NGUYỄN THỊ HIỀN <114793644+nguyenhienneu0706@users.noreply.github.com>
2023-06-23 15:52:39 +0700

    Update Learn_firebase-developers.sql

--- Learn_firebase-developers.sql
+++ Learn_firebase-developers.sql
@@ -213,7 +213,7 @@ FROM (
 ----------------------------------------------------------------------------------------------------------------------------------------
 
 -- TRƯỜNG HỢP GAMOTA:
-
+-- RR THEO TUẦN:
 WITH analytics_data AS (
   SELECT user_pseudo_id, event_timestamp, event_name, 
     UNIX_MICROS(TIMESTAMP("2023-05-29 00:00:00", "-7:00")) AS start_day,
@@ -261,4 +261,55 @@ FROM (
     (SELECT count(*) 
      FROM week_3_users 
      JOIN week_0_users USING (user_pseudo_id)) AS week_3_cohort
+);
+
+-- RR THEO NGÀY:
+WITH analytics_data AS (
+  SELECT user_pseudo_id, event_timestamp, event_name,
+    UNIX_MICROS(TIMESTAMP("2023-05-29 00:00:00", "-7:00")) AS start_day,
+    3600*1000*1000*24*1 AS one_day_micros
+  FROM `gab002.analytics_378566684.events_*`
+  WHERE _table_suffix BETWEEN '20230529' AND '20230627'
+)
+
+
+SELECT
+  IFNULL(day_0_cohort / NULLIF(day_0_cohort, 0), 0) AS day_0_pct,
+  IFNULL(day_1_cohort / NULLIF(day_0_cohort, 0), 0) AS day_1_pct,
+  IFNULL(day_2_cohort / NULLIF(day_0_cohort, 0), 0) AS day_2_pct,
+  IFNULL(day_3_cohort / NULLIF(day_0_cohort, 0), 0) AS day_3_pct
+FROM (
+  WITH day_3_users AS (
+    SELECT DISTINCT user_pseudo_id
+    FROM analytics_data
+    WHERE event_timestamp BETWEEN start_day+(3*one_day_micros) AND start_day+(4*one_day_micros)
+  ),
+  day_2_users AS (
+    SELECT DISTINCT user_pseudo_id
+    FROM analytics_data
+    WHERE event_timestamp BETWEEN start_day+(2*one_day_micros) AND start_day+(3*one_day_micros)
+  ),
+  day_1_users AS (
+    SELECT DISTINCT user_pseudo_id
+    FROM analytics_data
+    WHERE event_timestamp BETWEEN start_day+(1*one_day_micros) AND start_day+(2*one_day_micros)
+  ),
+  day_0_users AS (
+    SELECT DISTINCT user_pseudo_id
+    FROM analytics_data
+    WHERE event_name = 'first_open'
+      AND event_timestamp BETWEEN start_day AND start_day+(1*one_day_micros)
+  )
+  SELECT
+    (SELECT count(*)
+     FROM day_0_users) AS day_0_cohort,
+    (SELECT count(*)
+     FROM day_1_users
+     JOIN day_0_users USING (user_pseudo_id)) AS day_1_cohort,
+    (SELECT count(*)
+     FROM day_2_users
+     JOIN day_0_users USING (user_pseudo_id)) AS day_2_cohort,
+    (SELECT count(*)
+     FROM day_3_users
+     JOIN day_0_users USING (user_pseudo_id)) AS day_3_cohort
 );
