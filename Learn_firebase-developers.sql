9285efb9d2af04565eee564ee3d996b748db60cd
NGUYỄN THỊ HIỀN <114793644+nguyenhienneu0706@users.noreply.github.com>
2023-06-23 16:57:38 +0700

    Update Learn_firebase-developers.sql

--- Learn_firebase-developers.sql
+++ Learn_firebase-developers.sql
@@ -269,9 +269,9 @@ WITH analytics_data AS (
     UNIX_MICROS(TIMESTAMP("2023-05-29 00:00:00", "-7:00")) AS start_day,
     3600*1000*1000*24*1 AS one_day_micros
   FROM `gab002.analytics_378566684.events_*`
-  WHERE _table_suffix BETWEEN '20230529' AND '20230627'
+  WHERE _table_suffix BETWEEN '20230529' AND '20230627' 
+  -- Có hay không có dòng này cũng được nó chỉ xác định cái phần mà câu này nó tìm đến
 )
-
 
 SELECT
   IFNULL(day_0_cohort / NULLIF(day_0_cohort, 0), 0) AS day_0_pct,
@@ -312,4 +312,53 @@ FROM (
     (SELECT count(*)
      FROM day_3_users
      JOIN day_0_users USING (user_pseudo_id)) AS day_3_cohort
+);
+
+-- ĐÂY LÀ CÂU CODE LẤY RA RR1,3,7 CỦA MỘT NGÀY NÀO ĐÓ, NGÀY MÌNH PHẢI TỰ THAY ĐỔI
+WITH analytics_data AS (
+  SELECT user_pseudo_id, event_timestamp, event_name,
+    UNIX_MICROS(TIMESTAMP("2023-05-29 00:00:00", "-7:00")) AS start_day,
+    3600*1000*1000*24*1 AS one_day_micros
+  FROM `gab002.analytics_378566684.events_*`
+  WHERE _table_suffix BETWEEN '20230529' AND '20230627'
+)
+SELECT
+  IFNULL(day_0_cohort / NULLIF(day_0_cohort, 0), 0) AS day_0_pct,
+  IFNULL(day_1_cohort / NULLIF(day_0_cohort, 0), 0) AS day_1_pct,
+  IFNULL(day_3_cohort / NULLIF(day_0_cohort, 0), 0) AS day_3_pct,
+  IFNULL(day_7_cohort / NULLIF(day_0_cohort, 0), 0) AS day_7_pct
+FROM (
+  WITH day_7_users AS (
+    SELECT DISTINCT user_pseudo_id
+    FROM analytics_data
+    WHERE event_timestamp BETWEEN start_day+(7*one_day_micros) AND start_day+(8*one_day_micros)
+  ),
+  day_3_users AS (
+    SELECT DISTINCT user_pseudo_id
+    FROM analytics_data
+    WHERE event_timestamp BETWEEN start_day+(3*one_day_micros) AND start_day+(4*one_day_micros)
+  ),
+  day_1_users AS (
+    SELECT DISTINCT user_pseudo_id
+    FROM analytics_data
+    WHERE event_timestamp BETWEEN start_day+(1*one_day_micros) AND start_day+(2*one_day_micros)
+  ),
+  day_0_users AS (
+    SELECT DISTINCT user_pseudo_id
+    FROM analytics_data
+    WHERE event_name = 'first_open'
+      AND event_timestamp BETWEEN start_day AND start_day+(1*one_day_micros)
+  )
+  SELECT
+    (SELECT count(*)
+     FROM day_0_users) AS day_0_cohort,
+    (SELECT count(*)
+     FROM day_1_users
+     JOIN day_0_users USING (user_pseudo_id)) AS day_1_cohort,
+    (SELECT count(*)
+     FROM day_3_users
+     JOIN day_0_users USING (user_pseudo_id)) AS day_3_cohort,
+    (SELECT count(*)
+     FROM day_7_users
+     JOIN day_0_users USING (user_pseudo_id)) AS day_7_cohort
 );
